882a6e1a10c77bc73c7296058b7ad20f9d779e78
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-11-23 13:06:50 +0000

    The components init-function was not disabled, which caused it to register callbacks with non-existing callables.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3477 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_AmazonCloudServiceProvider/component.php
+++ components/SERIA_AmazonCloudServiceProvider/component.php
@@ -1,9 +1,10 @@
 <?php
-return;
+	return; /* Disabled!  (Remove both returns to enable!!!) */
 	SERIA_Base::addClassPath(SERIA_ROOT.'/seria/components/SERIA_AmazonCloudServiceProvider/classes/*.class.php');
 
 	function SERIA_AmazonCloudServiceProviderInit()
 	{
+		return; /* Disabled! */
 		SERIA_Hooks::listen(SERIA_CloudServiceHooks::GET_PROVIDERS, array('SERIA_AmazonCloudServiceProvider','getProviders'));
 		SERIA_Hooks::listen(SERIA_GuiHooks::EMBED, array('SERIA_AmazonCloudServiceProvider','guiEmbed'));
 	}
