12ae402f3b079a9ca5bfe18a42458710232860dc
Joakim <Joakim@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-11-23 12:45:45 +0000

    - Safely gives out a unique assosciation (GUID) to a given NamedObject - Safely retrieves a NamedObject given same GUID
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3476 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/classes/SERIA_NamedObjects.class.php
+++ platform/classes/SERIA_NamedObjects.class.php
@@ -19,6 +19,38 @@
 			$result = call_user_func_array(array($className, $method), $args);
 			return $result;
 		}
+		static function getPublicKey(SERIA_NamedObject $object)
+		{
+			$name = serialize($object->getObjectId());
+			$db = SERIA_Base::db();
+			$sql = 'SELECT * FROM {namedobjects} WHERE name=?';
+			try {
+				$row = $db->query($sql, array($name))->fetch(PDO::FETCH_ASSOC);
+			} catch (PDOException $e) {
+				if($e->getCode()==="42S02")
+				{
+					$db->exec("CREATE TABLE {namedobjects} (id INTEGER PRIMARY KEY, name VARCHAR(100), UNIQUE(name)) ENGINE=InnoDB");
+					$row = false;
+				}
+				else throw $e;
+			}
+			if($row)
+				return $row['id'];
+
+			$id = SERIA_Base::guid();
+			if($db->exec("INSERT INTO {namedobjects} (id, name) VALUES (?, ?)", array($id, $name)))
+				return $id;
+			throw new SERIA_Exception("I was unable to insert an id for this object.");
+		}
+
+		static function getInstanceByPublicKey($id)
+		{
+			$db = SERIA_Base::db();
+			$rs = $db->query("SELECT name FROM {namedobjects} WHERE id=?", array($id));
+			$name = $rs->fetch(PDO::FETCH_COLUMN, 0);
+			if($name) return self::getInstanceOf($name);
+			throw new SERIA_Exception("Instance not found", SERIA_Exception::NOT_FOUND);
+		}
 	}
 
 ?>
