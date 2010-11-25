c28c5d028e6d6fc390337d3a028c8061b8c3e21e
frode <frode@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-11-25 20:39:22 +0000

    - Removed unfinished component
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3492 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_CloudService/classes/SERIA_CloudServiceProvider.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-	interface SERIA_CloudServiceProvider
-	{
-		/**
-		*	Returns metadata about each account/provider
-		*	@return array array('serviceName'=>, 'accountName'=>, 'editUrl'=>)
-		*/
-		function getInfo();
-	}
--- components/SERIA_CloudService/common.php
+++
@@ -1,4 +0,0 @@
-<?php
-	require(dirname(__FILE__).'/../../main.php');
-	$gui = new SERIA_Gui(_t("Seria Platform Cloud Management service"));
-	$gui->activeMenuItem('controlpanel/settings/services/cloud');
