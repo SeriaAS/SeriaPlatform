8af65ea781463666c3a2e2a67aa38f745793b8da
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-11-25 14:54:38 +0000

    Show menu trees instead of dummy test data in outboard cp.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3487 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_Outboard/cp/comments.php
+++ components/SERIA_Outboard/cp/comments.php
@@ -3,22 +3,25 @@
 
 	$gui->activeMenuItem('controlpanel/outboard/comments');
 
-	$contents = '<h1 class="legend">'._t("Comments").'</h1>';
-
-	$statistics = '<h2>'._t('A few statistics').'</h2>'
-		.SERIA_GuiVisualize::integer(_t('Total threads'), 12334)
-		.SERIA_GuiVisualize::integer(_t('Number of user accounts'), 232315)
-		.SERIA_GuiVisualize::integer(_t('New comments today'), 3002);
-
-	$actions = '<h2>'._t('What do you want to do?').'</h2>
-<ul>
-	<li><a href="'.htmlspecialchars(SERIA_HTTP_ROOT.'/?route=outboard/comments/moderate').'">'._t('Approve comments').'</a></li>
-	<li><a href="comments/word-blacklist.php">'._t('Manage ban-words').'</a></li>
-</ul>';
+	ob_start();
+?>
+	<?php
+		$topCategories = $gui->getMenuItemsLevel(3);
+	?>
+	<h2><?php echo _t('What do you want to do?'); ?></h2>
+	<ul>
+	<?php
+		$items = array();
+		foreach ($topCategories as $id => $info) {
+			?>
+				<li><a href="<?php echo htmlspecialchars($info['url']); ?>"><?php echo $info['title']; ?></a></li>
+			<?php
+		}
+	?>
+	</ul>
+<?php
+	$contents .= ob_get_clean();
 	
-
-	$contents .= "<table><tr><td style='width: 300px'>".$statistics."</td><td>".$actions."</td></tr></table>";
-
 	$gui->contents($contents);
 
 	echo $gui->output();
--- components/SERIA_Outboard/cp/index.php
+++ components/SERIA_Outboard/cp/index.php
@@ -7,6 +7,86 @@
 	$contents = '<h1 class="legend">'._t("Outboard").'</h1>
 <p>'._t("Manage user generated content and other interactive features for your users.").'</p>';
 
+	ob_start();
+?>
+	<?php
+		$topCategories = $gui->getMenuItemsLevel(2);
+	?>
+	<h2><?php echo _t('What do you want to do?'); ?></h2>
+	<?php
+		$items = array();
+		foreach ($topCategories as $id => $info) {
+			ob_start();
+			?>
+				<div style='overflow: hidden; float: left;'>
+					<div style='float: left;'>
+						<img alt='[Icon]' src="<?php echo htmlspecialchars($info['icon']); ?>" />
+					</div>
+					<div style='float: left;'>
+						<h2><a href="<?php echo htmlspecialchars($info['url']); ?>"><?php echo $info['title'] ?></a></h2>
+						<?php
+							$subitems = $gui->getMenuItems($id);
+							if ($subitems) {
+								?>
+									<ul>
+										<?php
+											foreach ($subitems as $subitem) {
+												?>
+													<li><a href="<?php echo htmlspecialchars($subitem['url']); ?>"><?php echo $subitem['title']; ?></a></li>
+												<?php
+											}
+										?>
+									</ul>
+								<?php
+							} else {
+								?>
+									<ul><li><em><?php echo _t("No alternatives here..."); ?></em></li></ul>
+								<?php
+							}
+						?>
+					</div>
+				</div>
+			<?php
+			$items[] = ob_get_clean();
+		}
+		/*
+		 * Flat list of items: Will be shown in a two-column structure:
+		 */
+		$row = array();
+		$rows = array();
+		foreach ($items as $count => $item) {
+			if (count($row)) {
+				$row[] = $item;
+				$rows[] = $row;
+				$row = array();
+			} else {
+				$row[] = $item;
+			}
+		}
+		if ($row)
+			$rows[] = $row;
+		$col_styles = array(
+			'width: 300px;',
+			''
+		);
+		foreach ($rows as $row) {
+			?>
+				<div style='overflow: hidden;'>
+					<?php
+						foreach ($row as $col => $item) {
+							?>
+								<div style='float: left; overflow: hidden; <?php echo $col_styles[$col]; ?>'>
+									<?php echo $item; ?>
+								</div>
+							<?php
+						}
+					?>
+				</div>
+			<?php
+		}
+	?>
+<?php
+	$contents .= ob_get_clean();
 	$gui->contents($contents);
 
 	echo $gui->output();
