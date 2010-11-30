305aff2d19531d9b3eb26efb8f8165b7d591b63a
Joakim <Joakim@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-11-30 09:40:55 +0000

    - Quick fix, vital to HTTPS Requests via v2.php RPC. IExplore automatically adds no-cache headers if nothing else is specified, making all RPC calls fail from Flash
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3507 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/rpc/v2.php
+++ platform/rpc/v2.php
@@ -3,12 +3,19 @@
 *	API for remote access of functions in Seria Platform
 */
 	require('../../main.php');
+	SERIA_ProxyServer::privateCache(10);
 	/**
 	*	'e' = encoding: serialization method (php,json)
 	*	'c' = class: name for RPC Server object
 	*	'm' = class method
 	*	'0'..'100' = class method arguments
 	*/
+	$fp = fopen(dirname(__FILE__).'/log.txt', 'a+');
+
+	fwrite($fp, serialize(array($_GET, $_POST)));
+
+	fwrite($fp, "\n1");
+
 	if(isset($_REQUEST['e']))
 	{
 		switch($_REQUEST['e'])
@@ -21,6 +28,7 @@
 	}
 	else
 		$serialize = array('SERIA_Lib','publishPHP');
+	fwrite($fp, "\n2");
 		
 	try
 	{
@@ -28,22 +36,26 @@
 			throw new SERIA_Exception('Required parameter c (classname) not specified');
 		if(empty($_REQUEST['m']))
 			throw new SERIA_Exception('Required parameter m (methodname) not specified');
+	fwrite($fp, "\n3");
 		if(class_exists($_REQUEST['c']))
 		{
 			$callback = array($_REQUEST['c'], 'rpc_'.$_REQUEST['m']);
 			if(!is_callable($callback))
 				throw new SERIA_Exception('Unknown RPC method '.$_REQUEST['c'].'::'.$_REQUEST['m'].'.');
+	fwrite($fp, "\n4");
 
 			$r = new ReflectionClass($_REQUEST['c']);
 			if(!$r->implementsInterface('SERIA_RPCServer'))
 				throw new SERIA_Exception('The class '.$_REQUEST['c'].' does not implement SERIA_RPCServer.');
 
+	fwrite($fp, "\n5");
 			$arguments = array();
 			$i = 0;
 			while(isset($_REQUEST[$i]))
 				$arguments[] = $_REQUEST[$i++];
 
 			$result = call_user_func_array($callback, $arguments);
+	fwrite($fp, serialize(array($callback, $arguments, $serialize, $result)));
 			call_user_func($serialize, $result);
         }
 		else
@@ -51,9 +63,18 @@
 	}
 	catch (SERIA_RPCAuthenticationRequiredException $e)
 	{
+	fwrite($fp, serialize($e));
+
+	fclose($fp);
+
+
 		call_user_func($serialize, array('error' => $e->getMessage(), 'code' => 0, 'class' => get_class($e), 'please_authenticate' => true));
 	}
 	catch (Exception $e)
 	{
+	fwrite($fp, serialize($e));
+
+	fclose($fp);
+
 		call_user_func($serialize, array('error' => $e->getMessage(), 'code' => $e->getCode(), 'class' => get_class($e)));
 	}
