973a552dbfcaa9a44b571dbfb2aa116b8484c017
frode <frode@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-12-02 11:22:54 +0000

    - By: Joakim - Added default controlbar SeriaControlbar.swf - Added modules to FlashPlayer to allow custom controlbars to be used - Modules are still just added as flashVars, prone to change - Added function to generate flashVars from array of params - Replaced the <object> code returned by the getPlayer function to be more compatible with MSIE
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3528 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_FlashPlayer/classes/SERIA_FlashPlayer.class.php
+++ components/SERIA_FlashPlayer/classes/SERIA_FlashPlayer.class.php
@@ -3,21 +3,50 @@
 	class SERIA_FlashPlayer implements SERIA_RPCServer {
 
 		private $_object;
+		private $_modules;
 
 		public function __construct($object) {
 			$this->_object = $object;
 		}
 
+		public function addModule($moduleName, $moduleUrl) {
+			$this->_modules[$moduleName] = $moduleUrl;
+		}
+
 		public function output() {
 			//SERIA_ScriptLoader::loadScript('js/flowplayer-3.2.4.min.js');
-			return '<object id="SERIA_FlashPlayer" width="100%" height="100%" data="'.SERIA_HTTP_ROOT.'/seria/components/SERIA_FlashPlayer/bin/SeriaPlayer.swf" 
-					type="application/x-shockwave-flash">
+			$flashvars = array(
+				'debugMode' => ((SERIA_Base::isLoggedIn() && $_GET["debugMode"]) ? 'true' : ''),
+				'httpRoot' => urlencode(SERIA_HTTP_ROOT),
+				'objectKey' => SERIA_NamedObjects::getPublicKey($this->_object),
+				'tracker_code' => GoogleAnalyticsComponent::getGoogleAnalyticsId(),
+			);
 
-					<!-- load configuration from config.jsp -->
-					<param name="flashvars" value=\''.(($_GET["debugMode"] && SERIA_Base::isLoggedIn()) ? 'debugMode=true' : '').'&httpRoot='.urlencode(SERIA_HTTP_ROOT).'&amp;objectKey='.SERIA_NamedObjects::getPublicKey($this->_object).'\' />
-					<param name="movie" value="'.SERIA_HTTP_ROOT.'/seria/components/SERIA_FlashPlayer/bin/SeriaPlayer.swf" />
-					<param name="allowfullscreen" value="true" />
-				</object>';
+			foreach($this->_modules as $moduleName => $moduleURL) {
+				$flashvars[$moduleName] = urlencode($moduleURL);
+			}
+
+			return "<object width='100%' height='100%' id='SERIA_FlashPlayer'>
+					<param name='allowFullscreen' value='true' />
+					<param value='opaque'/>
+					<param name='movie' value='".SERIA_HTTP_ROOT."/seria/components/SERIA_FlashPlayer/bin/SeriaPlayer.swf' />
+					<param name='allowscriptaccess' value='always' />
+					<param name='quality' value='high' />
+					<param name='flashvars' value='".$this->_flashVarsToString($flashvars)."' />
+					<embed src='".SERIA_HTTP_ROOT."/seria/components/SERIA_FlashPlayer/bin/SeriaPlayer.swf?".$this->_flashVarsToString($flashvars)."'
+					        quality='high'
+					        width='100%'
+					        height='100%'
+					        name='SERIA_FlashPlayer'
+					        align='middle'
+					        play='true'
+					        loop='false'
+					        allowFullscreen='true'
+					        allowScriptAccess='always'
+					        type='application/x-shockwave-flash'
+					        pluginspage='http://www.adobe.com/go/getflashplayer'>
+				        </embed>
+				</object>";
 		}
 
 		public function generateConfig()
@@ -40,5 +69,17 @@
 			}
 
 			return $o->getVideoData();
+		}
+
+		protected function _flashVarsToString($flashvarsArray)
+		{
+			$flashvarString = '';
+
+			foreach($flashvarsArray as $key => $val) {
+				if(!empty($val) && ($val != '') && isset($val))
+					$flashvarString .= $key.'='.$val.'&';
+			}
+
+			return $flashvarString;
 		}
 	}
