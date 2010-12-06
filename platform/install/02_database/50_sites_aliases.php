e66da6bd235519d0b9da4b3e80d0ee3d0f466a1c
frode <frode@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-12-06 14:32:41 +0000

    - Update file lacked the correct apostrophe causing a fatal error when run
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3539 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/install/02_database/50_sites_aliases.php
+++ platform/install/02_database/50_sites_aliases.php
@@ -3,7 +3,7 @@ SERIA_Base::db()->exec('CREATE TABLE {sites_aliases} (
 	`id` INTEGER NOT NULL,
 	`siteId` INTEGER NOT NULL,
 	`domain` VARCHAR(100),
-	`domainType` ENUM('alias','forwarder'),
+	`domainType` ENUM(\'alias\',\'forwarder\'),
 	PRIMARY KEY(id),
 	INDEX(siteId),
 	UNIQUE(domain)
