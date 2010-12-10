92deac656b9cf9c85a165235b194acd537c6c1a2
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-12-10 12:57:45 +0000

    Use SERIA_Base::redirectTo instead of setting the Location-header directly. SERIA_Base::redirectTo has greater flexibility.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3553 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 	require_once(dirname(__FILE__)."/main.php");
 	SERIA_Base::user(NULL); /* Logout */
-	header('Location: '.SERIA_HTTP_ROOT);
+	SERIA_Base::redirectTo(SERIA_HTTP_ROOT);
 	die();
