b46028c9b75743216a78d3cc4c033b413535901a
oerestad <oerestad@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2010-12-14 11:57:42 +0000

    Translation files for SERIA_Outboard
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3585 d23b63ab-52c4-41c5-9713-938aad2d84f3

---
+++ lang/nb-NO/seria/components/SERIA_Outboard/classes/SERIA_Comment.class.php
@@ -0,0 +1,4 @@
+<?php $Giant_list_lang = array_merge($Giant_list_lang, array (
+  'cd76ed7750f028b71b54adffb7807cd5' => 'E-post',
+  '4e24d5a5634830dd5b0c34dc94178477' => 'Tittel',
+)); ?>
---
+++ lang/nb-NO/seria/components/SERIA_Outboard/classes/SERIA_CommentLog.class.php
@@ -0,0 +1,9 @@
+<?php $Giant_list_lang = array_merge($Giant_list_lang, array (
+  '77ac42022802d72ecc3816dac4a9addf' => 'Spam',
+  'ea4612d965a30569700bd7206be086a2' => 'Personangrep',
+  'af1aadf837a180510c6ac0cd48c9c654' => 'Rasisme',
+  '5092c76069867aa6a592094318bcbc17' => 'Pornografi',
+  '33c873ee2e90169c84148c10b6401dea' => 'Opphavsrett',
+  '7c864b6df0dd66d88aaabc0ec4247ead' => 'Annen grunn',
+  'dd172d8d3db566c99e29c1f23386321c' => 'Kommentar',
+)); ?>
