8c5d32d634b34133b58d149c26590e0f0368ea10
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2011-02-21 11:50:53 +0000

    Use a globally available row-template to allow sorting of columns in SERIA_MetaGrid.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3775 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_Outboard/cp/comments/browse.php
+++ components/SERIA_Outboard/cp/comments/browse.php
@@ -4,114 +4,6 @@
 	$gui->activeMenuItem('controlpanel/outboard/comments/browse');
 
 	$contents = '<h1 class="legend">'._t("Browse all comments").'</h1>';
-	function browseCommentFilter($row)
-	{
-		$editUrl = new SERIA_Url(SERIA_HTTP_ROOT.'/seria/apps/outboard/pages/comments/edit.php');
-		$editUrl->setParam('id', $row->get('id'));
-		$editUrl->setParam('return', SERIA_Url::current()->__toString());
-		ob_start();
-		?>
-			<tr onclick="location.href = <?php echo htmlspecialchars(SERIA_Lib::toJSON($editUrl->__toString())); ?>">
-				<td><?php echo $row->get('user'); ?></td>
-				<td>
-					<?php
-						$obj = $row->get('metaObject');
-						if ($obj instanceof SERIA_IWebLocation) {
-							$id = strip_tags($obj->getTitle());
-							if (strlen($id) > 15)
-								$id = substr($id, 0, 12).'...';
-							if (!$id) {
-								if (isset($obj['id']))
-									$id = $obj['id'];
-								else
-									$id = '--';
-							}
-							$resUrl = $obj->getUrl();
-						} else {
-							if (isset($obj['id']))
-								$id = $obj['id'];
-							else
-								$id = '--';
-							if (isset($obj['url']))
-								$resUrl = $obj['url'];
-							else
-								$resUrl = false;
-						}
-						if ($resUrl) {
-							$eid = 'eid'.mt_rand();
-							?>
-								<a id='<?php echo $eid; ?>' target='_blank' href="<?php echo htmlspecialchars($obj['url']); ?>"><?php echo $obj['url']; ?></a>
-								<script type='text/javascript'>
-									(function (element) {
-										var cancelBubble = function (e) {
-											if (!e)
-												var e = window.event;
-											e.cancelBubble = true;
-											if (e.stopPropagation)
-												e.stopPropagation();
-										}
-										if (element.addEventListener)
-											element.addEventListener('click', cancelBubble, false);
-										else
-											element.attachEvent('onclick', cancelBubble);
-									})(document.getElementById('<?php echo $eid; ?>'));
-								</script>
-							<?php
-						} else {
-							echo $id;
-						}
-					?>
-				</td>
-				<td><?php echo $row->get('title'); ?></td>
-				<td><?php 
-					if($row->get('flagged'))
-						echo _t("Flagged");
-					else if($row->get('approved'))
-						echo _t("Approved");
-					else if($row->get('rejected'))
-						echo _t("Low quality approved");
-					else if($row->get('hidden'))
-						echo _t('Rejected');
-					else
-						echo _t("Awaiting approval");
-				?></td>
-				<td>
-					<?php
-						$approveAction = $row->approveAction();
-						if ($approveAction->invoked()) {
-							if (!$approveAction->success)
-								SERIA_HtmlFlash::error(_t('Failed to approve comment!'));
-							SERIA_Base::redirectTo($approveAction->removeFromUrl(SERIA_Url::current())->__toString());
-							die();
-						}
-						$rejectAction = $row->rejectAction();
-						if ($rejectAction->invoked()) {
-							if (!$rejectAction->success)
-								SERIA_HtmlFlash::error(_t('Failed to reject comment!'));
-							SERIA_Base::redirectTo($rejectAction->removeFromUrl(SERIA_Url::current())->__toString());
-							die();
-						}
-						$deleteAction = $row->deleteAction();
-						if ($deleteAction->invoked()) {
-							if (!$deleteAction->success)
-								SERIA_HtmlFlash::error(_t('Failed to delete comment!'));
-							SERIA_Base::redirectTo($deleteAction->removeFromUrl(SERIA_Url::current())->__toString());
-							die();
-						}
-						if (!$row->get('approved')) {
-							?><a title="<?php echo htmlspecialchars(_t('Approve')); ?>" href="<?php echo htmlspecialchars($approveAction->__toString()); ?>"><span class='ui-icon ui-icon-check' style='float: left;'><?php echo _t('Approve'); ?></span></a><?php
-						}
-						if (!$row->get('hidden')) {
-							?><a title="<?php echo htmlspecialchars(_t('Reject')); ?>" href="<?php echo htmlspecialchars($rejectAction->__toString()); ?>"><span class='ui-icon ui-icon-cancel' style='float: left;'><?php echo _t('Reject'); ?></span></a><?php
-						}
-					?>
-					<a title="<?php echo htmlspecialchars(_t('Edit')); ?>" href="<?php echo htmlspecialchars($editUrl->__toString()); ?>"><span class='ui-icon ui-icon-pencil' style='float: left;'><?php echo _t('Edit'); ?></span></a>
-					<a onclick='return false;' name="<?php echo htmlspecialchars($row->get('title')); ?>" class='deleteAction' title="<?php echo htmlspecialchars(_t('Delete')); ?>" href="<?php echo htmlspecialchars($deleteAction->__toString()); ?>"><span class='ui-icon ui-icon-trash' style='float: left;'><?php echo _t('Delete'); ?></span></a>
-				</td>
-			</tr>
-		<?php
-		return ob_get_clean();
-	}
 
 	ob_start();
 	?>
@@ -191,7 +83,7 @@
 	}
 	$comments->order('-createdDate');
 	$grid = new SERIA_MetaGrid($comments);
-	$contents .= $grid->output(array('user' => 150, 'metaObject', 'title', 'approved' => 125, _t('Operations') => 100), 'browseCommentFilter');
+	$contents .= $grid->output(array('user' => 150, 'metaObject', 'title', 'approved' => 125, _t('Operations') => 100), array('SERIA_BrowseCommentsTemplate', 'browseCommentFilter'));
 
 	ob_start();
 	?>
