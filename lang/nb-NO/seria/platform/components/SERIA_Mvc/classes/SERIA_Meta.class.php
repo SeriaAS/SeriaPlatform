95c08ae1b8efdd3fb2ba76b6f5e32de407086447
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2011-02-22 12:11:41 +0000

    Translate some bits to nb-NO and nn-NO.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3783 d23b63ab-52c4-41c5-9713-938aad2d84f3

---
+++ lang/nb-NO/seria/platform/components/SERIA_Mvc/classes/SERIA_Meta.class.php
@@ -0,0 +1,8 @@
+<?php $Giant_list_lang = array_merge($Giant_list_lang, array (
+  '41cf0027d9e557e628005fcd1cc31208' => 'Registrering',
+  '8cc6b3f7063ccea8738710a562289a3b' => 'Registrert av',
+  '7a97f9d09083d1cb732b7489825f49f9' => 'Modifikasjon',
+  '3a5f716d08af0c4c820d2a5e73330a14' => 'Modifisert av',
+  'a5b43a9a1764835438c6cb9a615c5e1e' => 'Opprettet dato',
+  '904f7c77716b82df3dd0f6dfe658005e' => 'Sist endret dato',
+)); ?>
---
+++ lang/nn-NO/seria/platform/components/SERIA_Mvc/api/SERIA_MetaGrid.php
@@ -0,0 +1,3 @@
+<?php $Giant_list_lang = array_merge($Giant_list_lang, array (
+  'f3da46911df775ff97814d6b09882e49' => 'Kan ikkje sortere denne tabellen fordi templaten er ein funksjon som ikkje er tilgjengeleg fra globalt namespace (frå AJAX API)',
+)); ?>
