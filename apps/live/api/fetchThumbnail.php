2dfc5420e77af2585fbea2709611f00e8be863c1
joakim <joakim@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2011-03-03 15:54:06 +0000

    - Added check for file before generating new thumb
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3795 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- apps/live/api/fetchThumbnail.php
+++ apps/live/api/fetchThumbnail.php
@@ -1,14 +1,18 @@
 <?php
-        header("Content-Type: image/jpeg");
-
 	require_once(dirname(__FILE__).'/../../../main.php');
-	
+	if(!SERIA_Base::user()) throw new SERIA_Exception('Access denied', SERIA_Exception::ACCESS_DENIED);
+
+
 	$sec = intval($_GET['sec']);
         $filename = md5($_GET['rtmp']).'-'.(intval($sec/4)*4).'.jpg';
 
-        $rtmp = $_GET['rtmp'];
-        shell_exec("/usr/bin/rtmpdump -A ".$sec." -b ".($sec+1)." -r ".escapeshellarg($rtmp)." | /usr/local/bin/ffmpeg -i - -r 1 -f image2 ".SERIA_TMP_ROOT."/".$filename);
-        header("Content-Type: image/jpeg");
+	if(!file_exists(SERIA_TMP_ROOT.'/'.$filename))
+	{
+	        $rtmp = $_GET['rtmp'];
+        	shell_exec("/usr/bin/rtmpdump -A ".$sec." -b ".($sec+1)." -r ".escapeshellarg($rtmp)." | /usr/local/bin/ffmpeg -i - -r 1 -f image2 ".SERIA_TMP_ROOT."/".$filename);
+	}
 
-        readfile(SERIA_TMP_ROOT.'/'.$filename);
+	SERIA_Template::disable();
+        header("Content-Type: image/jpeg");
+	readfile(SERIA_TMP_ROOT.'/'.$filename);
 
