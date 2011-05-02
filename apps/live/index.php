e8a58b8fdfa829834c923bb92e64b5492e32491b
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2011-05-02 08:24:16 +0000

    Simultaniously remove live application from trunk, 0 and 0.2.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@3927 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- apps/live/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require("common.php");
-	$gui->title(_t("SeriaLIVE"));
-
-
-	$gui->contents("Deprecated");
-	echo $gui->output();
-
-?>
--- apps/live/presentations/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require('../common.php');
-
-	$gui->activeMenuItem('serialive/presentations');
-	$gui->title(_t("Presentations"));
-
-	$contents = '<h1 class="legend">'._t("Presentations").'</h1>';
-
-	echo $gui->contents($contents)->output();
