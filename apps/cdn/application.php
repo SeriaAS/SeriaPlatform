f00eb4c8993a67c1a3b7fc96d9c578993d54caad
frode <frode@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2011-08-10 11:34:00 +0000

    Listened to no-existing hook
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4045 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- apps/cdn/application.php
+++ apps/cdn/application.php
@@ -6,6 +6,7 @@
 		return;
 	require_once(SERIA_ROOT.'/_config.cdn.php');
 
+
 	if(!defined('SERIACDN_HOST'))
 	{
 		$pi = new SERIA_Url(SERIA_HTTP_ROOT);
@@ -19,6 +20,6 @@
 	*/
 	$seriaCDN = new SERIA_CDNApplication();
 	SERIA_Applications::addApplication($seriaCDN);
-	SERIA_Hooks::listen('seria_router', array($seriaCDN, 'router'), 1000);
+	SERIA_Hooks::listen(SERIA_PlatformHooks::ROUTER_FAILED, array($seriaCDN, 'router'), 1000);
 	SERIA_Hooks::listen('SERIA_UserEditForm', array($seriaCDN, 'userEdit'));
-
+	SERIA_Hooks::listen(SERIA_GuiManifest::EMBED_HOOK, array($seriaCDN, 'guiEmbed'));
