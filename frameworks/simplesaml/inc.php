77c8d1d8e3eb9bba454a62fc6c7be9b84fc73faa
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2012-02-13 08:53:09 +0000

    Default Simplesamlphp version up to 1.8.2.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4338 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- frameworks/simplesaml/inc.php
+++ frameworks/simplesaml/inc.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('LOAD_SIMPLESAMLPHP_VERSION'))
-	define('LOAD_SIMPLESAMLPHP_VERSION', '1.5.1');
+	define('LOAD_SIMPLESAMLPHP_VERSION', '1.8.2');
 
 require_once(dirname(__FILE__).'/classes/SimplesamlLibrary.class.php');
 
