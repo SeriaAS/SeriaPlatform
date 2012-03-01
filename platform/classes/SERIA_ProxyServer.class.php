b4a30cb81102917ec2811672d8fa5d903f42357c
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2012-03-01 10:18:00 +0000

    Add a sticky-mode to cache-control (SERIA_ProxyServer) where multiple calls to modify the cache-headers will be vombined to preserve the most restrictive cache-control.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4386 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/classes/SERIA_ProxyServer.class.php
+++ platform/classes/SERIA_ProxyServer.class.php
@@ -3,6 +3,23 @@
 	 *	Class for handling proxy servers that work in front of Seria Platform.
 	 */
 	class SERIA_ProxyServer {
+		protected static $cacheLim = false;
+		protected static $expireTime = false;
+		protected static $stickyMode = false;
+
+		/**
+		 *
+		 * Switches the sticky mode causing the cache-control
+		 * to remember calls to methods here and always use the most
+		 * restrictive cache-control. As opposed to always overriding
+		 * the current restrictions.
+		 * @param boolean $acc
+		 */
+		public static function stickyCacheControl($sticky=true)
+		{
+			self::$stickyMode = $sticky;
+		}
+
 		/**
 		 * Sends headers that informs the proxy server to never cache this page.
 		 */
@@ -15,6 +32,8 @@
 			// if session is started, then PHP will override the above headers with it's own headers using the following configuration
 			session_cache_limiter('nocache');
 			session_cache_expire(0);
+
+			self::$cacheLim = 'nocache';
 		}
 
 		/**
@@ -23,12 +42,26 @@
 		 */
 		public static function privateCache($ttl=60)
 		{
+			if (self::$stickyMode &&
+			    (self::$cacheLim == 'nocache' ||
+			     (self::$cacheLim == 'private' && self::$expireTime < $ttl))) {
+				/*
+				 * This has been overriden by a previous call to
+				 * nocache or private, meaning that parts of this response is
+				 * not cacheable, or privately cacheable with a lower ttl.
+				 * Thus not allowing caching of this response with this ttl.
+				 */
+				return;
+			}
 			header("Cache-Control: private, max-age=".intval($ttl).", s-maxage=".intval($ttl).", post-check=".(intval($ttl)/2).", pre-check=".(intval($ttl)));
 			header('Pragma: private');
 
 			// if session is started later, then PHP will override the above headers with it's own headers using the following configuration
 			session_cache_limiter('private');
 			session_cache_expire(intval($ttl / 60));
+
+			self::$cacheLim = 'private';
+			self::$expireTime = $ttl;
 		}
 
 		/**
@@ -39,6 +72,17 @@
 		 */
 		public static function publicCache($ttl=60)
 		{
+			if (self::$stickyMode &&
+			    (self::$cacheLim && self::$cacheLim != 'public' ||
+			     (self::$cacheLim == 'public' && self::$expireTime < $ttl))) {
+				/*
+				 * This has been overriden by a previous call to
+				 * nocache or private, meaning that parts of this response is
+				 * not cacheable, or privately cacheable with a lower ttl.
+				 * Thus not allowing caching of this response with this ttl.
+				 */
+				return;
+			}
 			header("Cache-Control: public, max-age=".intval($ttl).", s-maxage=".intval($ttl).", post-check=".intval($ttl).", pre-check=".(intval($ttl)*2));
 			header("Expires: " . gmdate('D, d M Y H:i:s \G\M\T', time() + intval($ttl)));
 			header("Pragma: public");
