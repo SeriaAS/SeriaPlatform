5c6d32b45eda519c85502445fd26d6440f88b9b1
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2012-04-30 13:45:03 +0000

    Add authinfo-api.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4505 d23b63ab-52c4-41c5-9713-938aad2d84f3

---
+++ components/Authproviders/classes/SAPI_ExtauthUser.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class SAPI_ExtauthUser extends SAPI
+{
+	/**
+	 *
+	 * Get a (guest) login-url returning back to continue.
+	 *
+	 * @param string $continue Continue-url.
+	 */
+	public static function loginUrl($continue)
+	{
+		return SERIA_User::loginUrl($continue)->__toString();
+	}
+	public static function logoutUrl($continue)
+	{
+		return SERIA_User::logoutAction($continue)->__toString();
+	}
+	public static function getUserData()
+	{
+		SERIA_ProxyServer::noCache();
+		$user = SERIA_Base::user();
+		if ($user !== false) {
+			$fields = array(
+				"firstName",
+				"lastName",
+				"displayName",
+				"username",
+				"email",
+				'is_administrator',
+				'guestAccount'
+			);
+			$values = array('loggedIn' => true, 'uid' => $user->get('id'));
+			foreach ($fields as $field)
+				$values[$field] = $user->get($field);
+			return $values;
+		} else
+			return array('loggedIn' => false);
+	}
+}
