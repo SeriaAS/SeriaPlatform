0cd6f2deacb5a6db41c66940cdc8f1eee689de5f
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2012-06-26 10:14:03 +0000

    SERIA_JsonUserData depends on common.js being loaded and therefore should resolve that dependency by calling script-loader.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4545 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_JsonUserData/component.php
+++ components/SERIA_JsonUserData/component.php
@@ -4,6 +4,7 @@ function SERIA_JsonUserData_init()
 {
 	SERIA_Base::addClassPath(dirname(__FILE__).'/classes/*.class.php');
 	SERIA_ScriptLoader::init();
+	SERIA_ScriptLoader::loadScript('SERIA-Platform-Common');
 	SERIA_Template::jsInclude(SERIA_HTTP_ROOT.'/seria/components/SERIA_JsonUserData/js/Cache.js');
 	SERIA_Template::jsInclude(SERIA_HTTP_ROOT.'/seria/components/SERIA_JsonUserData/js/User.js');
 	SERIA_Template::jsInclude(SERIA_HTTP_ROOT.'/seria/components/SERIA_JsonUserData/js/UserPropertyList.js?rev=2');
