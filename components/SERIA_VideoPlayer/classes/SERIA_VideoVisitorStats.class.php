f4ec7e0684ed3b3186b4b342152eae2b740a5a3a
joakim <joakim@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2012-07-17 20:22:10 +0000

    - Added result proportionSeen and updated helptext
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4577 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/SERIA_VideoPlayer/classes/SERIA_VideoVisitorStats.class.php
+++ components/SERIA_VideoPlayer/classes/SERIA_VideoVisitorStats.class.php
@@ -28,17 +28,19 @@ class SERIA_VideoVisitorStats extends SERIA_MetaObject implements SERIA_IApiAcce
 	public static function apiQuery($params)
 	{
 		if(isset($params['help'])) return array(
-			'info' => 'Fetch user-based videostatistics from Seria VideoPlayer. With no parameters, you will receive an error. Either the objectKey or an external unique identifier has to be set.',
+			'info' => 'Fetch user-based videostatistics from Seria VideoPlayer. With no parameters, you will receive an error. Either a videoId, its objectKey or an external unique identifier has to be set.',
 			'params' => array(
-				'videoid' => 'Fetch information given a single video',
+				'videoId' => 'Fetch information given a single video',
 				'objectKey' => 'Fetch information given by object key',
 				'euid' => 'External User Identificator, is the unique key set by the embedder from which the staistics is assosciated with',
-				'cat' => 'All videos in a given category id (/api/SERIA_VideoCategory)',
 				'start' => 'All videos starting at offset [start]',
 				'length' => 'Return at most [length] videos. Maximum value is 1000',
 				'order' => 'One of "publishedDate", "publishedDateDesc", "createdDate" or "createdDateDesc"',
 			),
 		);
+
+		if(!isset($params['euid']) && !isset($params['videoId']) && !isset($params['objectKey']))
+			throw new SERIA_Exception("EUID Required");
 
 		$videostats = SERIA_Meta::all('SERIA_VideoVisitorStats');
 
@@ -48,31 +50,33 @@ class SERIA_VideoVisitorStats extends SERIA_MetaObject implements SERIA_IApiAcce
 
 		$videostats->limit($params['start'], $params['length']);
 
-		if(isset($params['videoid'])) {
-			$videostats->where('video=:vid', array('vid' => $params['videoid']));
+		if(isset($params['videoId'])) {
+			$videostats->where('video=:vid', array('vid' => $params['videoId']));
 		} else if(isset($params['objectKey'])) {
 			$obj = SERIA_NamedObjects::getInstanceByPublicId($params['objectKey'], 'SERIA_Video');
 			$videostats->where('video=:vid', array('vid' => $obj->get("id")));
 		}
-		if(!isset($params['euid']) && !isset($params['videoid']))
-			throw new SERIA_Exception("EUID Required");
 
 		if(isset($params['euid']))
 			$videostats->where('euid=:e', array('e' => $params['euid']));
 
 		$result = array();
 		foreach($videostats as $videostat) {
-			$strc = substr_count($videostat->get("seenMap"), 1);
-			$percentSeen = intval(($strc/strlen($videostat->get("seenMap")))*100);
+			$seenMap = $videostat->get("seenMap");
+			$seenMap[0] = 1; // Hack, all seenmaps begin with 0
+			$strc = substr_count($seenMap, 1);
+			$percFloat = ($strc/strlen($seenMap));
+			$percentSeen = round($percFloat*100);
 			if($percentSeen>100)
 				$percentSeen = 100;
 			$result[] = array(
-				'videoid' => $videostat->get("video")->get("id"),
+				'videoId' => $videostat->get("video")->get("id"),
 				'objectKey' => SERIA_NamedObjects::getPublicId($videostat->get("video")),
 				'title' => $videostat->get("video")->get("title"),
 				'euid' => $videostat->get("euid"),
-				'seenMap' => $videostat->get("seenMap"),
+				'seenMap' => $seenMap,
 				'percentSeen' => $percentSeen,
+				'proportionSeen' => round($percFloat, 4)
 			);
 		}
 		return $result;
