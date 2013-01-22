abd1cff3e0ba92172d4e5c5f510fdde627c5d1d8
joakim <joakim@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2013-01-22 14:15:10 +0000

    - Frode? Added something needed on prod for seriawebcast to prevent crashes. seria excpetions can now hold extra html info to show on error pages
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4742 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/classes/SERIA_Exception.class.php
+++ platform/classes/SERIA_Exception.class.php
@@ -1,6 +1,8 @@
 <?php
 	class SERIA_Exception extends Exception
 	{
+		public $extra = NULL;		// Holds extra information about this error - use HTML
+
 		const NOT_FOUND = 404;		// NOT FOUND EXCEPTION CODE
 		const ACCESS_DENIED = 503;	// ACCESS DENIED
 		const INCORRECT_USAGE = 3;	// INCORRECT USAGE
@@ -8,4 +10,9 @@
 		const NOT_READY = 5;		// NOT READY
 		const DISABLED = 6;		// THE FEATURE IS NOT ENABLED IN THIS CASE
 		const UNSUPPORTED = 7;		// THE OPERATION REQUIRED AN UNSUPPORTED FEATURE
+
+		public function __construct($message=NULL, $code=NULL, $extra=NULL) {
+			parent::__construct($message, $code);
+			$this->extra = $extra;
+		}
 	}
