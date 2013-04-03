8af4e5f3cf00dcc99be13b775cb63341f43f7e0a
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2013-04-03 06:10:48 +0000

    Add SSO by js proof of concept test.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4792 d23b63ab-52c4-41c5-9713-938aad2d84f3

---
+++ tests/components/Authproviders/ssobyjs.php
@@ -0,0 +1,8 @@
+<?php
+	require(dirname(__FILE__).'/../../../main.php');
+?><!DOCTYPE html>
+<head>
+	<title>Test SSO by js</title>
+	<script type='text/javascript' src="<?php echo htmlspecialchars(SERIA_HTTP_ROOT.'/?route=components/authproviders/ssobyjs&siteType=test'); ?>"></script>
+</head>
+<p>...</p>
