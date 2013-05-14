2554dd125a86ffc6ba32e2a08398c63c26490449
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2013-05-14 09:10:06 +0000

    Add manifest with dependency-info and init-function.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4852 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/NDLA_RoamAuthCookie/component.php
+++ components/NDLA_RoamAuthCookie/component.php
@@ -1,8 +1,30 @@
 <?php
 
 if (defined('NDLA_ROAM_AUTH_COOKIE') && NDLA_ROAM_AUTH_COOKIE) {
-	SERIA_Base::addClassPath(dirname(__FILE__)."/classes/*.class.php");
-	$component = new NDLA_RoamAuthCookieComponent(NDLA_ROAM_AUTH_COOKIE);
-	SERIA_Components::addComponent($component);
-	$component->embed();
+	/**
+	 *
+	 *
+	 * @author Jan-Espen Pettersen
+	 * @package authndla
+	 *
+	 */
+	class NDLA_RoamAuthCookieManifest
+	{
+		const SERIAL = 1;
+		const NAME = 'NDLA_RoamAuthCookie';
+	
+		public static $classPaths = array(
+			'classes/*.class.php'
+		);
+		public static $dependencies = array(
+			'RoamAuthprovider'
+		);
+	}
+
+	function NDLA_RoamAuthCookieInit()
+	{
+		$component = new NDLA_RoamAuthCookieComponent(NDLA_ROAM_AUTH_COOKIE);
+		SERIA_Components::addComponent($component);
+		$component->embed();
+	}
 }
