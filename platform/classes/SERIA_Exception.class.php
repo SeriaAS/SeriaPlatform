2c91ee910fb547e8f9a8b458c736920a7562f226
frode <frode@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2013-08-26 14:10:50 +0000

    - Changed the error code for access denied to the correct HTTP error code.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@4984 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- platform/classes/SERIA_Exception.class.php
+++ platform/classes/SERIA_Exception.class.php
@@ -4,7 +4,7 @@
 		public $extra = NULL;		// Holds extra information about this error - use HTML
 
 		const NOT_FOUND = 404;		// NOT FOUND EXCEPTION CODE
-		const ACCESS_DENIED = 503;	// ACCESS DENIED
+		const ACCESS_DENIED = 401;	// ACCESS DENIED
 		const INCORRECT_USAGE = 3;	// INCORRECT USAGE
 		const NOT_IMPLEMENTED = 4;	// NOT IMPLEMENTED
 		const NOT_READY = 5;		// NOT READY
