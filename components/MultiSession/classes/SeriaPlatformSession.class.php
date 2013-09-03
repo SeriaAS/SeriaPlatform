1a777d85164ac94a682c666938d7fb0ffa91df53
janespen <janespen@d23b63ab-52c4-41c5-9713-938aad2d84f3>
2013-09-03 07:58:22 +0000

    Expire old unmarked sessions after max 30 seconds, unless activity is detected. And only if there is a logged in user.
    git-svn-id: https://svn.open.seria.net/svn/platform/trunk/seria@5000 d23b63ab-52c4-41c5-9713-938aad2d84f3

--- components/MultiSession/classes/SeriaPlatformSession.class.php
+++ components/MultiSession/classes/SeriaPlatformSession.class.php
@@ -24,23 +24,25 @@ class SeriaPlatformSession extends OfflineSession
 	}
 	public function sessionGC()
 	{
-		$expiry = ini_get('session.gc_maxlifetime');
-		$expiry_lim = time() - $expiry;
-		$unset_lim = 1800;
-		if ($unset_lim > ini_get('session.gc_maxlifetime'))
-			$unset_lim = ini_get('session.gc_maxlifetime');
-		$unset_lim += $expiry;
-		if ($this->exists(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX)) {
-			if ($this->get(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX) < $expiry_lim) {
-				/*
-				 * Session has expired!
-				 */
-				$this->clearAll();
+		if ($this->exists(SERIA_PREFIX.'_USERID'.SERIA_SESSION_SUFFIX)) {
+			$expiry = ini_get('session.gc_maxlifetime');
+			$expiry_lim = time() - $expiry;
+			$unset_lim = 1800;
+			if ($unset_lim > ini_get('session.gc_maxlifetime'))
+				$unset_lim = ini_get('session.gc_maxlifetime');
+			$unset_lim = time() - $unset_lim;
+			if ($this->exists(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX)) {
+				if ($this->get(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX) < $expiry_lim) {
+					/*
+					 * Session has expired!
+					 */
+					$this->clearAll();
+					$this->save();
+				}
+			} else {
+				$this->set(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX, $unset_lim);
 				$this->save();
 			}
-		} else {
-			$this->set(SERIA_PREFIX.'_USERTIME'.SERIA_SESSION_SUFFIX, $unset_lim);
-			$this->save();
 		}
 	}
 }
